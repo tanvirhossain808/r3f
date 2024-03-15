db221e75bd76babc819df14e3c7f2be4be445cd4
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-15 19:26:34 +0600

    pushed the debug things

--- r3f-debug/src/Experience.jsx
+++ r3f-debug/src/Experience.jsx
@@ -1,8 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+import { useControls, button,/* folder = is function to create folder inside folder*/ } from 'leva'
+import { Perf } from 'r3f-perf';
 
 export default function Experience() {
-    const { position, color } = useControls({
+
+    const { perfVisible } = useControls({
+        perfVisible: true
+    })
+    const { position, color, visible } = useControls("sphere", {
         position: {
             /*  value: -2,*/
             value: { x: -2, y: 0 },
@@ -11,24 +16,45 @@ export default function Experience() {
             step: 0.01,
             joystick: "invertY"
         },
-        color: "#ff0000"
+        color: "#ff0000",
+        visible: true,
+        myInterval: {
+            min: 0,
+            max: 10,
+            value: [4, 5]
+        },
+        clickMe: button(() => {
+            console.log('hey');
+        }),
 
+        choice: {
+            options: ["a", "b", "c"]
+        }
+    })
+
+    const { scale } = useControls('cube', {
+        scale: {
+            value: "1.5",
+            step: "0.01",
+            min: 0,
+            max: 5
+        }
     })
     return <>
-
+        {perfVisible ? <Perf position='top-left' /> : null}
         <OrbitControls makeDefault />
 
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <mesh position={[position.x, position.y, 0]}>
+        <mesh position={[position.x, position.y, 0]} visible={visible}>
             <sphereGeometry />
             <meshStandardMaterial color={color} />
         </mesh>
 
         {/* <Cube scale={2} /> */}
 
-        <mesh position-x={2} scale={1.5}>
+        <mesh position-x={2} scale={scale} >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
